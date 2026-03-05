b254d971a2f263c52b1c9884e90bc40b28102c8e
termi <pablo.san14@educa.madrid.org>
2026-03-05 12:04:05 +0100

    Relleno de atributos finalizado

--- src/main/java/org/termi/Airline.java
+++ src/main/java/org/termi/Airline.java
@@ -52,7 +52,6 @@ public class Airline {
     }
 
     public Set<Passengers> getPassengersByFlightNumber(int flightnumber){
-        rellenoDeAtributos();
         IndexFlights indexFlights1 = indexFlights.get(flightnumber);
         if (indexFlights1 != null){
             return indexFlights1.getPassengers();
@@ -60,7 +59,6 @@ public class Airline {
         return null;
     }
     public List<Clients> getClientsByFlightNumber(int flightnumber){
-        rellenoDeAtributos();
         IndexFlights indexFlights1 = indexFlights.get(flightnumber);
         if (indexFlights1 == null){
             return null;
@@ -70,12 +68,10 @@ public class Airline {
                         .filter(client -> client.getNif().equals(passengers.getNif()))).toList();
     }
     public List<Clients> findClientsBySurname(Airline airline, String surname){
-        rellenoDeAtributos();
         return airline.getClients().stream()
                 .filter(c -> c.getSurname().equals(surname)).collect(Collectors.toList());
     }
     public Integer seatByFlightAndNif(Airline airline, int flightNumber, String nif){
-        rellenoDeAtributos();
         IndexFlights flights = airline.getIndexFlights().get(flightNumber);
         if (flights == null){
             return  null;
@@ -88,7 +84,6 @@ public class Airline {
         return null;
     }
     public void addLuggageToPassenger(Airline airline, int flightNumber, Luggages luggages, String nif){
-        rellenoDeAtributos();
         IndexFlights flights = airline.getIndexFlights().get(flightNumber);
 
         if (flights == null){
@@ -109,7 +104,6 @@ public class Airline {
         passengersFounded.getLuggages().add(luggages);
     }
     public List<IndexFlights> flightsByNifWhereClientIsPassenger (Airline airline, String nif){
-        rellenoDeAtributos();
         List<IndexFlights> clients = new ArrayList<>();
         for (IndexFlights flights : airline.getIndexFlights().values()){
             for (Passengers passengers : flights.getPassengers()){
